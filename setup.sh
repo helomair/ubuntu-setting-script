d458216989f35ee4029fba16f562de3a468e0b65
helo <helo@terpro.com>
2024-12-05 16:57:52 +0800

    Refactor

--- setup.sh
+++ setup.sh
@@ -1,109 +1,137 @@
 #!/bin/bash
-# Program:
-#       Ubuntu setup.
-# Auther:
-#       Helomair
-# 2020-2-7
 
 SUDO=''
 if (($EUID != 0)); then
 	SUDO='sudo'
 fi
 
-# Install packages
-echo "Installing packages"
-$SUDO apt update
-$SUDO apt install -y git vim tmux zsh powerline fonts-powerline build-essential curl make gcc g++ clang zoxide ripgrep fd-find yarn lldb python3-pip python3-venv
-$SUDO apt install -y python3-dev docker.io cmake exuberant-ctags fzf ninja-build gettext unzip
-$SUDO apt install -y php8.1 php8.1-dev php8.1-gd php8.1-mbstring php8.1-xml php8.1-curl php8.1-mysql php8.1-fpm
+install_packages() {
+	echo "Installing packages"
+	$SUDO apt update
+	$SUDO apt install -y git vim tmux zsh powerline fonts-powerline build-essential curl make gcc g++ clang zoxide ripgrep fd-find yarn lldb python3-pip python3-venv
+	$SUDO apt install -y python3-dev docker.io cmake exuberant-ctags fzf ninja-build gettext unzip
+	$SUDO apt install -y php8.1 php8.1-dev php8.1-gd php8.1-mbstring php8.1-xml php8.1-curl php8.1-mysql php8.1-fpm
+}
 
-# Move to HOME
-cd ~
+download_configs() {
+    if [ ! -d "~/ubuntu_settings_backup" ]; then
+        echo "Start download configs from Helomair/ubuntu_settings_backup"
+        git clone https://github.com/Helomair/ubuntu_settings_backup.git
+    fi
+}
 
-# If configs not download yet, download them.
-if [ ! -d "~/ubuntu_settings_backup" ]; then
-	echo "Start download configs from Helomair/ubuntu_settings_backup"
-	git clone https://github.com/Helomair/ubuntu_settings_backup.git
-fi
+setup_vim() {
+    echo "Start setup vim"
+    cp ~/ubuntu_settings_backup/.vimrc ~/.vimrc
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    vim --cmd 'source ~/.vimrc' -c 'qa!'
+    vim --cmd 'PlugInstall' -c 'qa!'
 
-# Setup vim
-echo "Start setup vim"
-cp ~/ubuntu_settings_backup/.vimrc ~/.vimrc
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim --cmd 'source ~/.vimrc' -c 'qa!'
-vim --cmd 'PlugInstall' -c 'qa!'
+    cd ~/.vim/bundle/YouCompleteMe
+    python3 install.py
+    cd ~
+}
 
-# Compile Plugin YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-./install.sh --clang-completer
+setup_neovim() {
+    echo "Start setup neovim"
 
-cd ~
+    ## nvim appimage
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x nvim.appimage
+    ln -s ~/nvim.appimage /usr/bin/nvim
 
-# Setup neovim
-## nvim appimage
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-ln -s ~/nvim.appimage /usr/bin/nvim
+    ## bob, nvim version manager
+    curl https://sh.rustup.rs -sSf | sh
+    source "$HOME/.cargo/env"
+    cargo install --git https://github.com/MordechaiHadad/bob.git
+    bob install nightly
+    bob use nightly
 
-## bob, nvim version manager
-curl https://sh.rustup.rs -sSf | sh
-source "$HOME/.cargo/env"
-cargo install --git https://github.com/MordechaiHadad/bob.git
-bob install nightly
-bob use nightly
+    ### nvimdots, nvim configuration
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+    ### nvm
+    curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+    nvm install 18
+    nvm use 18
 
-### nvimdots, nvim configuration
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-### nvm
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-nvm install 18
-nvm use 18
+    if command -v curl >/dev/null 2>&1; then
+        bash -c "$(curl -fsSL https://raw.githubusercontent.com/ayamir/nvimdots/HEAD/scripts/install.sh)"
+    else
+        bash -c "$(wget -O- https://raw.githubusercontent.com/ayamir/nvimdots/HEAD/scripts/install.sh)"
+    fi
 
-if command -v curl >/dev/null 2>&1; then
-	bash -c "$(curl -fsSL https://raw.githubusercontent.com/ayamir/nvimdots/HEAD/scripts/install.sh)"
-else
-	bash -c "$(wget -O- https://raw.githubusercontent.com/ayamir/nvimdots/HEAD/scripts/install.sh)"
-fi
+    yes | cp -rf ~/ubuntu_settings_backup/nvimdots_user_settings_backup/* .config/nvim/lua/user
+}
 
-yes | cp -rf ~/ubuntu_settings_backup/nvimdots_user_settings_backup/* .config/nvim/lua/user
+setup_tmux() {
+    echo "Start setup tmux, using oh-my-tmux."
+    git clone https://github.com/gpakosz/.tmux.git
 
-# Setup Oh-My-Tmux
-echo "Start setup tmux, using oh-my-tmux."
-git clone https://github.com/gpakosz/.tmux.git
-cp ~/ubuntu_settings_backup/.tmux.conf .tmux/.tmux.conf
-ln -s -f .tmux/.tmux.conf
-cp ~/ubuntu_settings_backup/.tmux.conf.local ~/.tmux.conf.local
+    cp ~/ubuntu_settings_backup/.tmux.conf .tmux/.tmux.conf
+    ln -s -f .tmux/.tmux.conf
 
-# Setup Composer & Laravel
-#echo "Start setup composer & laravel."
-# Install Composer
-#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-#php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-#php composer-setup.php
-#php -r "unlink('composer-setup.php');"
+    cp ~/ubuntu_settings_backup/.tmux.conf.local ~/.tmux.conf.local
 
-# Require Laravel Intaller
-#composer global require laravel/installer
-#export PATH="$HOME/.config/composer/vendor/bin:$PATH"
+    # TPM, tmux plugin manager
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}
 
-# Test Laravel Installer
-#if [ ! -d "~/mygit " ]; then
-#    echo "Dir mygit not found, make it."
-#    mkdir ~/mygit
-#fi
-#cd ~/mygit
-#laravel new laravel-test
+setup_composer() {
+    echo "Start setup composer"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+}
 
-# Setup Zsh
-echo "Start setup zsh, using oh-my-zsh."
-curl -Lo install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sh install.sh
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+setup_laravel() {
+    echo "Start setup laravel."
+    composer global require laravel/installer
+    export PATH="$HOME/.config/composer/vendor/bin:$PATH"
 
-cp ~/ubuntu_settings_backup/.zshrc ~/.zshrc
-# source ~/.zshrc
-exec /bin/zsh
+    # Test Laravel Installer
+    # if [ ! -d "~/mygit " ]; then
+    #    echo "Dir mygit not found, make it."
+    #    mkdir ~/mygit
+    # fi
+    # cd ~/mygit
+    # laravel new laravel-test
+}
+
+setup_zsh() {
+    echo "Start setup zsh, using oh-my-zsh."
+    curl -Lo install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+    sh install.sh
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+    cp ~/ubuntu_settings_backup/.zshrc ~/.zshrc
+    exec /bin/zsh
+}
+
+
+main() {
+    cd ~
+
+    download_configs
+
+    install_packages
+
+    setup_vim
+    # setup_neovim
+
+    setup_tmux
+
+    setup_composer
+    setup_laravel
+
+    setup_zsh
+}
+
+main
+
+
+
